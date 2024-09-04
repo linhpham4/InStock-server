e3b0a44aba03371c51d2d5aa54ef03bc2485f196
Kyle Ingham <kyleingy@proton.me>
2024-09-04 13:32:17 -0600

    Added route for delete.  It has been successfully tested.

--- routes/inStockRoutes.js
+++ routes/inStockRoutes.js
@@ -1,6 +1,20 @@
 import express from "express";
 const router = express.Router();
-import fs from "fs";
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
 
+router.delete("/warehouse/:warehouseId", async (req, res) => {
+  const id = req.params.warehouseId;
+  try {
+    const selectedItem = await knex("warehouses").where("id", id).del();
+
+    console.log(selectedItem);
+    res.status(200).json(selectedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json("Warehouse not found");
+  }
+});
 
 export default router;
