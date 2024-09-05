8bfc46db8745801f8dcf1b53ce38b1c45bdaea06
Linh Pham <linh.p44@gmail.com>
2024-09-05 09:30:11 -0700

    add get request for a single warehouse to routes

--- routes/inStockRoutes.js
+++ routes/inStockRoutes.js
@@ -10,9 +10,13 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
-router.route("/warehouses/:warehouseId").delete(warehouseController.remove);
+router.route("/warehouses/:warehouseId")
+    .delete(warehouseController.remove)
+    .get(warehouseController.findWarehouse)
 
-router.route('/warehouses').post(warehouseController.addNew);
+router
+    .route('/warehouses')
+    .post(warehouseController.addNew);
 
 
 
