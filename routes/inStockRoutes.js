12299f39159fff36e0e101742deec634805c9b6a
linhpham4 <linh.p44@gmail.com>
2024-09-05 11:24:30 -0500

    Revert "Feature J2E-14"

--- routes/inStockRoutes.js
+++ routes/inStockRoutes.js
@@ -1,22 +1,6 @@
 import express from "express";
-import initKnex from "knex";
-import configuration from "../knexfile.js";
-const knex = initKnex(configuration);
-
-import * as warehouseController from "../controllers/warehouse-controller.js";
-
-
 const router = express.Router();
-
-
-router
-    .route("/warehouses/:warehouseId")
-    .delete(warehouseController.remove);
-
-router
-    .route('/warehouses')
-    .get(warehouseController.getWarehouses)
-    .post(warehouseController.addNew);
+import fs from "fs";
 
 
 export default router;
