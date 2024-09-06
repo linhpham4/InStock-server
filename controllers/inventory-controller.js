cdfbb4163410a37da860dbdcdd392dfbdd7a455f
ryanmody <rahimmody488@gmail.com>
2024-09-06 11:12:26 -0400

    Update logic so that inventorySingle returns only the columns specified in the ticket

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -31,24 +31,33 @@ const getSingleInventory = async (req,res) => {
         const inventorySingle = await knex('inventories')
         .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
         .where({ 'inventories.id':id })
+        .select(
+          'inventories.id',
+          'warehouse_name',
+          'item_name',
+          'description',
+          'category',
+          'status',
+          'quantity'
+      )
 
         if (inventorySingle.length === 0) {
           res.status(404).json("Inventory item not found");
           return;
         }
         
-        await knex('inventories')
-        .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
-        .where({ 'inventories.id':id })
-        .select(
-            'inventories.id',
-            'warehouse_name',
-            'item_name',
-            'description',
-            'category',
-            'status',
-            'quantity'
-        )
+      //  await knex('inventories')
+      //   .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
+      //   .where({ 'inventories.id':id })
+      //   .select(
+      //       'inventories.id',
+      //       'warehouse_name',
+      //       'item_name',
+      //       'description',
+      //       'category',
+      //       'status',
+      //       'quantity'
+      //   )
     res.status(200).json(inventorySingle[0]) 
     } catch (error) {
         res.status(500).send(`Unable to retrieve data for inventory item with ID ${req.params.itemId}`)
