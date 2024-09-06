7194b59caf90844b94328e1a205d18ff4744493e
ryanmody <rahimmody488@gmail.com>
2024-09-06 11:14:48 -0400

    Update logic so that inventorySingle returns only the columns specified in the ticket

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -3,31 +3,56 @@ import configuration from "../knexfile.js";
 
 const knex = initKnex(configuration);
 
+const removeSingleInventory = async (req, res) => {
+  const id = req.params.itemId;
+
+  try {
+    const inventoryItem = await knex("inventories")
+      .where({ id });
+    if (inventoryItem.length === 0) {
+      res.status(404).json("Inventory item not found");
+      return;
+    }
+    await knex('inventories')
+      .where({ id })
+      .del();
+    res.status(200).json(inventoryItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(`Bad request. ${error}`);
+  }
+};
+
 const getSingleInventory = async (req,res) => {
 
-    try {
-        const id  = req.params.itemId
+  try {
+      const id  = req.params.itemId
 
-        const inventorySingle = await knex('inventories')
-        .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
-        .where({ 'inventories.id':id })
-        .select(
-            'inventories.id',
-            'warehouse_name',
-            'item_name',
-            'description',
-            'category',
-            'status',
-            'quantity'
-        )
+      const inventorySingle = await knex('inventories')
+      .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
+      .where({ 'inventories.id':id })
+      .select(
+        'inventories.id',
+        'warehouse_name',
+        'item_name',
+        'description',
+        'category',
+        'status',
+        'quantity'
+      )
 
-
-    res.status(200).json(inventorySingle) 
-    } catch (error) {
-        res.status(500).send(`Unable to retrieve data for inventory item with ID ${req.params.itemId}`)
-    }
+      if (inventorySingle.length === 0) {
+        res.status(404).json("Inventory item not found");
+        return;
+      }
+      
+  res.status(200).json(inventorySingle[0]) 
+  } catch (error) {
+      res.status(500).send(`Unable to retrieve data for inventory item with ID ${req.params.itemId}`)
+  }
 }
 
+// Get all inventory
 const getAll = async (req, res) => {
   try {
     const inventory = await knex("inventories")
@@ -51,6 +76,7 @@ const getAll = async (req, res) => {
   }
 };
 
+// Edit/update an existing resource in its entirety (PUT)
 const edit = async (req, res) => {
   const id = req.params.itemId;
   const request = req.body;
@@ -80,4 +106,4 @@ const edit = async (req, res) => {
   }
 };
 
-export { getAll, getSingleInventory, edit};
+export { getAll, getSingleInventory, edit, removeSingleInventory};
