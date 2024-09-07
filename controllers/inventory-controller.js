cae80295363880c5e78a045df0f283756d35836e
Linh Pham <linh.p44@gmail.com>
2024-09-07 12:30:44 -0700

    add validation for missing properties, inventory id not found, warehouse id not found, and quantity not a number. Fix response body to return correct data type

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -8,49 +8,44 @@ const removeItem = async (req, res) => {
   const id = req.params.itemId;
 
   try {
-    const inventoryItem = await knex("inventories")
-      .where({ id });
+    const inventoryItem = await knex("inventories").where({ id });
     if (inventoryItem.length === 0) {
       return res.status(404).json(`Item with ID ${id} not found`);
     }
-    await knex('inventories')
-      .where({ id })
-      .del();
-    res.status(204).end()
+    await knex("inventories").where({ id }).del();
+    res.status(204).end();
   } catch (error) {
     res.status(500).json(`${error}`);
   }
 };
 
-
 // Get an inventory item
-const getItem = async (req,res) => {
-
+const getItem = async (req, res) => {
   try {
-      const id  = req.params.itemId
+    const id = req.params.itemId;
 
-      const inventorySingle = await knex('inventories')
-      .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
-      .where({ 'inventories.id':id })
+    const inventorySingle = await knex("inventories")
+      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+      .where({ "inventories.id": id })
       .select(
-        'inventories.id',
-        'warehouse_name',
-        'item_name',
-        'description',
-        'category',
-        'status',
-        'quantity'
-      )
+        "inventories.id",
+        "warehouse_name",
+        "item_name",
+        "description",
+        "category",
+        "status",
+        "quantity"
+      );
 
-      if (inventorySingle.length === 0) {
-        return res.status(404).json(`Item with ID ${id} not found`);
-      }
-      
-  res.status(200).json(inventorySingle[0]) 
+    if (inventorySingle.length === 0) {
+      return res.status(404).json(`Item with ID ${id} not found`);
+    }
+
+    res.status(200).json(inventorySingle[0]);
   } catch (error) {
     res.status(500).json(`${error}`);
   }
-}
+};
 
 // Get all inventory
 const getAllItems = async (_req, res) => {
@@ -76,29 +71,47 @@ const getAllItems = async (_req, res) => {
 // Edit/update an existing resource in its entirety (PUT)
 const editItem = async (req, res) => {
   const id = req.params.itemId;
-  const request = req.body;
+  
   try {
-    if (
-      request.id &&
-      request.warehouse_id &&
-      request.item_name &&
-      request.description &&
-      request.category &&
-      request.status &&
-      request.quantity
-    ) {
-      await knex("inventories").where({ id }).update(req.body);
-      const updatedInventory = await knex("inventories").where({ id });
-      res.status(200).json(updatedInventory);
-    } else {
-      res
-        .status(500)
-        .json(
-          "Bad request. All edit requests must have all keys with non-empty values"
-        );
 
-        // return res.status(404).json(`Item with ID ${id} not found`);
+    if (
+      !req.body.warehouse_id ||
+      !req.body.item_name ||
+      !req.body.description ||
+      !req.body.category ||
+      !req.body.status ||
+      req.body.quantity === undefined
+    ) {
+      return res.status(400).json("Please provide all required item information");
     }
+
+    const warehouses = await knex("warehouses");
+    if (!warehouses.find((warehouse) => warehouse.id === req.body.warehouse_id)) {
+      return res.status(400).json(`Warehouse with ID ${req.body.warehouse_id} not found`);
+    }
+
+    if (typeof req.body.quantity !== "number") {
+      return res.status(400).json("Quantity must be a number");
+    }
+
+    await knex("inventories").where({ id }).update(req.body);
+    const updatedInventory = await knex("inventories")
+      .where({ id })
+      .select(
+        "id",
+        "warehouse_id",
+        "item_name",
+        "description",
+        "category",
+        "status",
+        "quantity"
+      )
+
+    if (updatedInventory.length === 0) {
+      return res.status(404).json(`Item with ID ${id} not found`);
+    }
+      
+    res.status(200).json(updatedInventory[0]);
   } catch (error) {
     res.status(500).json(`${error}`);
   }
@@ -110,21 +123,28 @@ const addNewItem = async (req, res) => {
     // checks that request contains all required data
     ////// !req.body.quantity will not work for cases where quantity is zero (as !0 is truthy)
     ////// so we need to check if it's undefined
-    if (!req.body.warehouse_id || !req.body.item_name || !req.body.description || !req.body.category || !req.body.status || req.body.quantity === undefined ) {
+    if (
+      !req.body.warehouse_id ||
+      !req.body.item_name ||
+      !req.body.description ||
+      !req.body.category ||
+      !req.body.status ||
+      req.body.quantity === undefined
+    ) {
       return res.status(400).json("Please provide all required item information");
-    };
-    
+    }
+
     // checks if a warehouse in the warehouses table has an id matching warehouse_id from the request
     const warehouses = await knex("warehouses");
     if (!warehouses.find((warehouse) => warehouse.id === req.body.warehouse_id)) {
       return res.status(400).json(`Warehouse with ID ${req.body.warehouse_id} not found`);
-    };
+    }
 
     // checks if the value of "quantity" is a number
     if (typeof req.body.quantity !== "number") {
       return res.status(400).json("Quantity must be a number");
-    };
-    
+    }
+
     const updatedInventory = await knex("inventories").insert(req.body);
     const newItemId = updatedInventory[0];
     const newItem = await knex("inventories")
@@ -143,6 +163,6 @@ const addNewItem = async (req, res) => {
   } catch (error) {
     res.status(500).json(`${error}`);
   }
-}
+};
 
-export { getAllItems, getItem, editItem, removeItem, addNewItem};
+export { getAllItems, getItem, editItem, removeItem, addNewItem };
